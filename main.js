b5ac38996557e57aec9907f31c3cecb2d5f431a0
wynterwolf <shinyguns@gmail.com>
2020-12-10 14:46:25 -0600

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,42 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
-
+document.addEventListener("DOMContentLoaded", () => {
+  // hidden model
+  const model = document.getElementById("modal")
+  model.className = 'hidden'
+  // gather ALL the hearts by class name
+  const hearts = document.getElementsByClassName("like-glyph")
+  // console.log(hearts)
+  // add event listener to each heart (iterate over collection)
+  for (const heart of hearts) {
+    // pass in event to use that
+    heart.addEventListener("click", (e) => {
+      // make server call - success = change the heart
+      // if empty make it full, add new class for this
+      // if full make empty
+      mimicServerCall() //see code below for explanation
+      // when successful change the heart status
+        .then(() => {
+          if (heart.innerHTML == EMPTY_HEART) {
+            heart.innerHTML = FULL_HEART
+            heart.className = "activated-heart"
+          } else {
+            heart.innerHTML = EMPTY_HEART
+            heart.className = "like-glyph"
+          }
+        })
+        .catch(error => {
+          model.hidden = false
+          const modalMessage = document.querySelector("#modal-message")
+          modalMessage.innerText = error
+          setTimeout(() => { 
+            modal.hidden = true
+          }, 5000)
+      })
+    })
+  }
+})
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
